c82de4313295130b093be91438f9046a84675ece
Shishir <shishirchaurasiya435@gmail.com>
2024-06-27 23:02:18 +0530

    fix: socket.io 404

--- server/index.ts
+++ server/index.ts
@@ -38,7 +38,7 @@ nextApp.prepare().then(async ()=>{
 
     app.all("*",(req:any,res:any)=>nextHandler(req,res))
 
-    app.listen(port,()=>{
+    server.listen(port,()=>{
         console.log(`Server is running on http://localhost:${port}`)
     })
 })
