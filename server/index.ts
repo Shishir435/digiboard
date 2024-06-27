04e16ac5ac81e5145f5aa7a6a3e05ee0136c2d98
Shishir435 <shishirchaurasiya435@gmail.com>
2024-06-27 12:06:57 +0530

    add new socketlisteners

--- server/index.ts
+++ server/index.ts
@@ -27,10 +27,21 @@ nextApp.prepare().then(async ()=>{
     io.on("connection",(socket)=>{
         console.log("connection")
 
+        socket.join("global")
+
+        const allUsers=io.sockets.adapter.rooms.get("global")
+        if(allUsers) io.emit("users_in_room",[...allUsers])
+
         socket.on("draw",(moves,option)=>{
             console.log("drawing")
             socket.broadcast.emit("socket_draw",moves,option)
         })
+
+        socket.on("mouse_move",(x,y)=>{
+            console.log("mouse_move",x,y)
+            socket.broadcast.emit("mouse_moved",x,y,socket.id)
+        })
+
         socket.on("disconnect",()=>{
             console.log("client disconnected")
         })
