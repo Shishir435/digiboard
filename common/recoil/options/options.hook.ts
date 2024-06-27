5ffce1c4adad82fcc9e9df0d9530f663e1426d4f
Shishir435 <shishirchaurasiya435@gmail.com>
2024-06-27 20:08:01 +0530

    add setOptions hook

--- common/recoil/options/options.hook.ts
+++ common/recoil/options/options.hook.ts
@@ -1,8 +1,13 @@
-import { useRecoilValue } from "recoil"
+import { useRecoilValue, useSetRecoilState } from "recoil"
 import { optionsAtom } from "./options.atom"
 
 
 export const useOptions=()=>{
     const option=useRecoilValue(optionsAtom)
     return option
+}
+
+export const useSetOptions=()=>{
+    const setOption=useSetRecoilState(optionsAtom)
+    return setOption
 }
