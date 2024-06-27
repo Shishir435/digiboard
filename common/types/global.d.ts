e5472eb216a6c1506ec3e516a39d9b00d066a78c
Shishir435 <shishirchaurasiya435@gmail.com>
2024-06-27 14:31:19 +0530

    fix typo in global.d.ts

--- common/types/global.d.ts
+++ common/types/global.d.ts
@@ -5,13 +5,15 @@ export declare global {
     }
     
     export interface ServerToClientEvents {
-        socket_draw: (newMoves:[number,number][],options:CtxOptions) => void;
+        user_draw: (newMoves:[number,number][],options:CtxOptions,userId:string) => void;
         mouse_moved: (x:number,y:number,socketId:string)=>void;
         users_in_room: (socketIds: string[])=>void;
+        user_disconnected: (socketId: string)=>void
     }
     
     export interface ClientToServerEvents {
         draw: (moves:[number,number][],options:CtxOptions) => void;
         mouse_move: (x:number,y:number)=>void;
+        undo: ()=>void;
     }
 }
