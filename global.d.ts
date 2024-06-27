ee254bfdbbfb050eb57d1ee8a2722fc5029917ce
Shishir435 <shishirchaurasiya435@gmail.com>
2024-06-27 12:22:32 +0530

    add global.d.ts for type inference

---
+++ global.d.ts
@@ -0,0 +1,17 @@
+export declare global {
+    export interface CtxOptions {
+        lineWidth: number,
+        lineColor: string,
+    }
+    
+    export interface ServerToClientEvents {
+        socket_draw: (newMoves:[number,number][],options:CtxOptions) => void;
+        mouse_moved: (x:number,y:number,socketId:string)=>void;
+        users_in_room: (socketIds: string[])=>void;
+    }
+    
+    export interface ClientToServerEvents {
+        draw: (moves:[number,number][],options:CtxOptions) => void;
+        mouse_move: (x:number,y:number)=>void;
+    }
+}
