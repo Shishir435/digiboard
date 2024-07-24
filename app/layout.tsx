3b5add4919f6332d7c1bbcf2e71469b75ba04129
Shishir435 <shishirchaurasiya435@gmail.com>
2024-07-24 16:32:25 +0530

    add popins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,23 @@ import { Metadata } from "next";
 import React from "react";
 import "../styles/globals.css";
 import RecoidContextProvider from "@/modules/room/context/recoil.contextProvider";
+import {Poppins} from "next/font/google"
 export const metadata: Metadata = {
   title: "digiboard",
   description: "Next.js whiteboard application",
 };
+const poppins=Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100','200','300','400','500','600','700','800','900']
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>
         <RecoidContextProvider>{children}</RecoidContextProvider>
       </body>
