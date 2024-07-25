cb33183e9efe58dc43a054352977e36687787346
Shishir435 <shishirchaurasiya435@gmail.com>
2024-07-25 17:46:35 +0530

    remove history buttons

--- modules/room/components/toolbar/ToolBar.tsx
+++ modules/room/components/toolbar/ToolBar.tsx
@@ -5,11 +5,9 @@ import ColorPicker from "./ColorPicker";
 import BackgroundPicker from "./BackgroundPicker";
 import DownloadButton from "./DownloadButton";
 import ExitButton from "./ExitButton";
-import HistoryBtns from "./HistrotyBtns";
 import ImagePicker from "./ImagePicker";
 import LineWidthPicker from "./LineWidthPicker";
 import ModePicker from "./ModePicker";
-import SettingsButton from "./SettingsButton";
 import ShapeSelector from "./ShapeSelector";
 import ShareButton from "./ShareButton";
 
@@ -17,28 +15,18 @@ const ToolBar = () => {
   return (
     <div
       style={{ translate: "-50%" }}
-      className="absolute top-5 left-1/2 z-50 flex items-center gap-4 rounded-lg bg-toolbar px-4 py-2 text-white shadow-lg"
+      className="absolute top-5 left-1/2 z-[30] flex items-center gap-4 rounded-lg bg-toolbar px-4 py-2 text-toolbar-foreground shadow-lg"
     >
-      <HistoryBtns />
-
-      {/* <div className="h-px w-full bg-white 2xl:hidden" /> */}
-      <div className="w-px h-5 bg-gray-500" />
-
       <ShapeSelector />
       <ColorPicker />
       <LineWidthPicker />
       <ModePicker />
       <ImagePicker />
-
-      {/* <div className="2xl:hidden"></div> */}
-      {/* <div className="h-px w-full bg-white 2xl:hidden" /> */}
       <div className="w-px h-5 bg-gray-500" />
-
       <BackgroundPicker />
       <ShareButton />
       <DownloadButton />
       <ExitButton />
-      <SettingsButton />
     </div>
   );
 };
