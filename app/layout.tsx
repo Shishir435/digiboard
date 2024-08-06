49b99e14753a880be51fcd27cdfa41aeac438a95
Shishir435 <shishirchaurasiya435@gmail.com>
2024-08-06 19:58:54 +0530

    fix:link preload warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const poppins = Poppins({
   display: "swap",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["italic", "normal"],
+  preload: false
 });
 export default function RootLayout({
   children,
